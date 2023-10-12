9e6ed6bb0543551728592d8114cfaa1dcd9155a6
Nathaniel Hill <hello@nathanielhill.io>
2023-10-12 16:07:42 -0500

    Update entrypoint.sh
    Add more detailed output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ git config --system --add safe.directory /github/workspace
 
 if [ -z "$(git status --porcelain)" ];
 then
+  echo "No changes detected in working directory..."
   exit 0
 else
+  echo "Changes detected in working directory..."
+  echo "$(git status)"
   exit 1
 fi
